afbd2b8e52f379eb204b5afa27d4df4491cda4b1
Khanh Tran <khanhtranngoccva@gmail.com>
2024-09-17 16:43:11 +0700

    feat: Add intrinsic entropy reducer

--- winloader/winloader-entropy.h
+++ winloader/winloader-entropy.h
@@ -3,9 +3,12 @@
 #include <cstdlib>
 #include "intrin.h"
 
+#define __WINLOADER_ENTROPY_STUB__ 1
+
 namespace winloader {
     inline int entropy_stub_rand_arg = abs(rand()) - 20;
 
+#if (__WINLOADER_ENTROPY_STUB__)
     inline __forceinline void entropy_stub(int arg = winloader::entropy_stub_rand_arg) {
         if (arg <= 2147483646) {
             goto out;
@@ -45,4 +48,8 @@ namespace winloader {
         out:
         return;
     }
+#else
+    inline __forceinline void entropy_stub(int arg = winloader::entropy_stub_rand_arg) {
+    }
+#endif
 }
